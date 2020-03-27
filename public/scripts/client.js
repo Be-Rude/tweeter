0245bc8f4f22b79791a9cc6cc9457069528e46d9
Brad Ruud <bradruud@gmail.com>
2020-03-27 22:31:40 +0000

    add AJAX POST request to add new tweets to db

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -19,7 +19,7 @@ const data = [
     "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-    "created_at": 1461116232227
+    "created_at": 1585300854
   },
   {
     "user": {
@@ -29,7 +29,7 @@ const data = [
     "content": {
       "text": "Je pense , donc je suis"
     },
-    "created_at": 1461113959088
+    "created_at": 1585121854
   }
 ]
 
@@ -61,7 +61,6 @@ $(() => {
 
 const createTweetElement = function(tweet) {
  const $article = $('<article>').addClass('timeline')
- const $form = $('<form>');
  const $header = $('<header>').addClass('timeline-header');
  const $avatar = $('<div>').addClass('avatar');
  const $pic = $('<img>').attr('src', tweet.user.avatars);
@@ -75,16 +74,25 @@ const createTweetElement = function(tweet) {
  const $retweet = $('<i>').addClass('fas fa-retweet');
  const $heart = $('<i>').addClass('fas fa-heart');
 
- let unix_timestamp = tweet.created_at
- let actualdate = new Date(unix_timestamp * 1000); 
- let hours = actualdate.getHours();
- let days = hours / 24;
+ let currentTime = Math.floor(Date.now() / 1000);
+ let unix_timestamp = tweet.created_at 
+ let elapsedTime = currentTime - unix_timestamp;
+ let seconds = +elapsedTime;
+ let minutes = Math.floor(seconds / 60);
+ let hours = Math.floor(minutes / 60);
+ let days = Math.floor(hours / 24);
 
  let $time = 0;
- if (hours < 24) {
+ if (days >= 1) {
+  $time = $('<p>').text(days + " days ago");
+ } else 
+ if (hours >= 1) {
    $time = $('<p>').text(hours + " hours ago");
  } else
-  $time = $('<p>').text(days + " days ago");
+ if (minutes >= 1) {
+  $time = $('<p>').text(minutes + " minutes ago");
+ } else
+ $time = $('<p>').text(seconds + " seconds ago");
 
  $header.append($avatar);
  $header.append($pic);
@@ -109,4 +117,17 @@ const renderTweets = function(tweets) {
 
 renderTweets(data);
 
-});
+const $form = $('form');
+
+
+ $form.on('submit', (event) => {
+  event.preventDefault();
+  const formData = $form.serialize();
+
+  $.post('/tweets', formData)
+  .then((res) => {
+    console.log(res);
+  });
+  $('#tweet-text').val(''); 
+})
+})
