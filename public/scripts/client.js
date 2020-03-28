f9e76feb04bcba4e95853a5c585a1cbf42c49a7a
Brad Ruud <bradruud@gmail.com>
2020-03-28 00:23:41 +0000

    add JQuery GET function which imports tweet database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,55 +8,6 @@
 //   console.log(event);
 // });
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1585300854
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1585121854
-  }
-]
-
-/*{ <section class="timeline">
-        <form>
-          <header class="timeline-header">
-            <div class="avatar">
-            <img src="https://i.imgur.com/nlhLi3I.png">
-            <p class="person">Joline</p>
-            </div>
-            <div class="user">@joline</div>
-          </header>
-          
-          <p class="tweetContent">
-            If I have seen further it is by standing on the shoulders of giants (or possibly just regular tall dudes)
-          </p>
-          <footer class="footer">
-            <p >10 days ago</p>
-            <div>
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-            </div>
-          </footer>
-        </form>
-      </section> }*/
-
 $(() => {
 
 const createTweetElement = function(tweet) {
@@ -68,16 +19,15 @@ const createTweetElement = function(tweet) {
  const $handle  = $('<div>').addClass('user').text(tweet.user.handle);
  const $content = $('<p>').addClass('tweetContent').text(tweet.content.text);
  const $footer = $('<footer>').addClass('footer');
-//  const $time = $('<p>').text(tweet.created_at);
  const $div = $('<div>')
  const $flag = $('<i>').addClass('fas fa-flag');
  const $retweet = $('<i>').addClass('fas fa-retweet');
  const $heart = $('<i>').addClass('fas fa-heart');
 
  let currentTime = Math.floor(Date.now() / 1000);
- let unix_timestamp = tweet.created_at 
+ let unix_timestamp = Math.floor(tweet.created_at) / 1000;
  let elapsedTime = currentTime - unix_timestamp;
- let seconds = +elapsedTime;
+ let seconds = Math.floor(elapsedTime);
  let minutes = Math.floor(seconds / 60);
  let hours = Math.floor(minutes / 60);
  let days = Math.floor(hours / 24);
@@ -115,12 +65,12 @@ const renderTweets = function(tweets) {
   }
 }
 
-renderTweets(data);
+// renderTweets(data);
 
 const $form = $('form');
 
 
- $form.on('submit', (event) => {
+$form.on('submit', (event) => {
   event.preventDefault();
   const formData = $form.serialize();
 
@@ -129,5 +79,15 @@ const $form = $('form');
     console.log(res);
   });
   $('#tweet-text').val(''); 
+});
+
+
+const loadtweets = function() {
+$.getJSON('/tweets') 
+.then((res) => {
+  renderTweets(res);
 })
+}
+loadtweets();
+
 })
